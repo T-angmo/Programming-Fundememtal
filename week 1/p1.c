3213c78b23f55669cf4310723115560450e9540c
T-angmo <88903243+T-angmo@users.noreply.github.com>
2021-08-14 02:02:00 +0700

    Create p1.c

---
+++ week 1/p1.c
@@ -0,0 +1,38 @@
+// #include<stdio.h>
+// int main ()
+// {
+//     float a,b,c;
+//     scanf("%f %f %f", &a, &b, &c);
+//     if(a>b) {
+//         if(a>c) {
+//             printf("%f", a);
+//         }
+//         else {
+//             printf("%f", c);
+//         }
+//     }
+//     else if(b>a) {
+//         if(b>c) {
+//             printf("%f", b);
+//         }
+//         else {
+//             printf("%f", c);
+//         }
+//     }
+//     else {
+//         printf("%f", c);
+//     }
+
+//     return 0;
+
+// }
+
+#include<stdio.h>
+int main()
+{
+    float a,b,c;
+    scanf("%f %f %f", &a, &b, &c);
+    
+
+    return 0;
+}
